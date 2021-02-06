fa395e447e2c514623f3f56131b2a8eaff34b6dc
Ryan <ryan.w.boehm@gmail.com>
2021-02-06 13:13:02 -0500

    added lexical bindings for event handlers in the constructor

--- src/components/edit-todo.component.js
+++ src/components/edit-todo.component.js
@@ -5,6 +5,12 @@ export default class EditTodo extends Component {
   constructor(props) {
         super(props);
 
+        this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
+        this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
+        this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
+        this.onChangeTodoCompleted = this.onChangeTodoCompleted.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
         this.state = {
             todo_description: '',
             todo_responsible: '',
