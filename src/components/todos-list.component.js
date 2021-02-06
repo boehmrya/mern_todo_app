3b82c0bc1d0bfced05d3b378b84edee37bf087ed
Ryan <ryan.w.boehm@gmail.com>
2021-02-06 11:34:12 -0500

    added completed class css styling

--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+const Todo = props => (
+    <tr>
+        <td>{props.todo.todo_description}</td>
+        <td>{props.todo.todo_responsible}</td>
+        <td>{props.todo.todo_priority}</td>
+        <td>
+            <Link to={"/edit/"+props.todo._id}>Edit</Link>
+        </td>
+    </tr>
+)
+
 export default class TodosList extends Component {
+
     constructor(props) {
         super(props);
         this.state = {todos: []};
@@ -16,6 +28,12 @@ export default class TodosList extends Component {
             .catch(function (error){
                 console.log(error);
             })
+    }
+
+    todoList() {
+        return this.state.todos.map(function(currentTodo, i){
+            return <Todo todo={currentTodo} key={i} />;
+        })
     }
 
     render() {
